ad22b39ec177e9445b49f3e3e96e7a7e7ad9d497
pamelazoe <pmlzoe@gmail.com>
2020-08-28 17:37:26 -0500

    Removing unused code

--- src/App.js
+++ src/App.js
@@ -52,68 +52,16 @@ const App = () => {
     // eslint-disable-next-line
   }, [consult]);
 
-  // useEffect(() => {
-  //   const getWeather = async () => {
-  //     if (consult) {
-  //       const app_id = process.env.REACT_APP_APP_ID;
-  //       const api_uri = process.env.REACT_APP_API_URI;
-  //       const url = `${api_uri}q=${city},${country}&appid=${app_id}`;
-  //       const incomingData = await fetch(url);
-  //       const result = await incomingData.json();
-  //       setResult(result);
-  //       setConsult(false);
-  //       if (result.cod === "404") {
-  //         setError(true);
-  //       } else {
-  //         setError(false);
-  //       }
-  //     }
-  //   };
-  //   getWeather();
-  //   // eslint-disable-next-line
-  // }, [consult]);
-
-  // useEffect(() => {
-  //   const getImage = async () => {
-  //     if (consult) {
-  //       const up_acc_key = process.env.REACT_APP_UP_ACC_KEY;
-  //       const up_uri = process.env.REACT_APP_UP_API_URI;
-  //       const img_url = `${up_uri}?client_id=${up_acc_key}&query=${city}&location=${city} ${country}&orientation=landscape`;
-  //       const incomingImg = await fetch(img_url);
-  //       const img = await incomingImg.json();
-  //       img.urls ? setImage(img.urls) : setError(true);
-  //       console.log(incomingImg);
-  //     }
-  //   };
-  //   getImage();
-  //   // eslint-disable-next-line
-  // }, [consult]);
-
-  // const searchData = (data) => {
-  //   if (data.city === "" || data.country === "") {
-  //     //error
-  //     setError(true);
-  //     setResult("");
-  //     return;
-  //   }
-  //   // Si city y country existen, agregarlos al state
-
-  //   //  setCity(data.city);
-  //   //  setCountry(data.country);
-  //   console.log(city, country);
-  //   setError(false);
-  // };
 
   // Cargar un componente condicionalmente
   let component1;
   if (!error) {
-    // Si hay un error, mostrar el componente Error
+    //Mostrar el clima
     component1 = <Weather result={result} />;
 
   } else {
-    //Mostrar el clima
+    // Si hay un error, mostrar el componente Error
     component1 = <Error message="There are no results for this search" />;
-
   }
   let component2;
   if (noImage) {
